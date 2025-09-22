814b8b4ab259214573ec1312f99841eb2ea7b20b
RyeongGyun <ryungkyun123@gmail.com>
2025-09-22 17:18:20 +0900

    String 타입

---
+++ StrType.dart
@@ -0,0 +1,18 @@
+void main() {
+  String name = '김령균';
+  String name2 = "김령균";
+  String name3 = "김" + '령' + "균";
+  String name4 = '''
+  김령균  
+  김령균2
+  ''';
+  String name5 = """
+김령균
+김령균2
+""";
+print(name);
+print(name2);
+print(name3);
+print(name4);
+print(name5);
+}
